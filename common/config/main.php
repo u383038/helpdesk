4182e46606dc46da072c0a9169d00d3c235dae99
Solodkov A <antonsolodkov66@gmail.com>
2019-10-04 22:37:15 +0300

    first init

---
+++ common/config/main.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.mail.ru',
+                'username' => 'firm.clip@bk.ru',
+                'password' => 'uxNCGj4ob',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+];
